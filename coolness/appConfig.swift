6180fa829a346c6be661399b03d794d82b0683f3
Tamir <door2k@gmail.com>
2014-10-06 18:31:00 +0300

    added config tab, supporting save configuration

---
+++ coolness/appConfig.swift
@@ -0,0 +1,57 @@
+//
+//  appConfig.swift
+//  coolness
+//
+//  Created by Tamir Berliner on 10/6/14.
+//  Copyright (c) 2014 Tamir Berliner. All rights reserved.
+//
+
+import Foundation
+
+
+// BRAINLESS: just use appConfig(["key" : "value"]) and it will automatically be remembered and saved in app
+//            storage + remembered as default. use a key: appConfig().params["key"].
+//            TO ADD/UPDATE you must use the respective methods .add .updates
+
+class appConfig {
+    var params: [String : String] = [
+        "username"  : "test",
+        "server"    : "10.0.0.47",
+        "port"      : "5000",
+        "region"    : "Home",
+        "test"      : "1"
+    ]
+    
+    init (defaultValues: [String:String]) {
+        
+        for (key, value) in defaultValues {
+            self.params[key] = value
+        }
+
+        for (key, value) in self.params {
+            if NSUserDefaults().valueForKey(key) == nil {
+                NSUserDefaults().setValue(value, forKey: key)
+            } else {
+                params[key] = (NSUserDefaults().valueForKey(key) as String)
+            }
+        }
+    }
+    
+    convenience init () {
+        self.init(defaultValues: [:])
+    }
+    
+    func add(key: String, value: String) {
+        self.update(key, value: value)
+    }
+    
+    func update(key: String, value: String) {
+        NSUserDefaults().setValue(value, forKey: key)
+    }
+    
+    func debug() {
+        for (key, value) in params {
+            NSLog("\(key) : \(value)")
+        }
+    }
+}
