5e14655f9c3b954df5e1ce81141ade1b59e91635
Tamir <door2k@gmail.com>
2014-10-14 20:40:02 +0300

    Added list of visited apps as future support for running apps

---
+++ coolness/AppListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AppTableViewController.swift
+//  coolness
+//
+//  Created by Tamir Berliner on 10/13/14.
+//  Copyright (c) 2014 Tamir Berliner. All rights reserved.
+//
+
+import Foundation
---
+++ coolness/CNClasses.swift
@@ -0,0 +1,9 @@
+//
+//  appClass.swift
+//  coolness
+//
+//  Created by Tamir Berliner on 10/12/14.
+//  Copyright (c) 2014 Tamir Berliner. All rights reserved.
+//
+
+import Foundation
